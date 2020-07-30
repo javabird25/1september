ddde6c7bd2768fa73fa341f31821ba75f650ea66
javabird25 <shockck84@gmail.com>
2020-07-30 16:33:48 +0500

    Диалог с ссылкой для скачивания

--- resources/views/composition/setup.blade.php
+++ resources/views/composition/setup.blade.php
@@ -14,7 +14,13 @@
         @endforeach
     </div>
     <div class="send-wrapper">
-        <button>Готово</button>
+        <button id="done" onclick="compositionDone()" disabled>Готово</button>
+    </div>
+
+    <div class="dialog-darken">
+        <dialog>
+            <a id="download" download="Фото с рамкой.png">Скачать</a>
+        </dialog>
     </div>
 
     <script src="/js/compose.js"></script>
