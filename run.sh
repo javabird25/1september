89315a7f3bb2d3664907417b6f6506f02271abd1
javabird25 <shockck84@gmail.com>
2020-08-09 11:48:03 +0500

    Создание суперпользователя при запуске контейнера

--- run.sh
+++ run.sh
@@ -8,6 +8,18 @@ wait-for-it db:5432
 ./manage.py makemigrations --no-input
 ./manage.py migrate --no-input
 
+if [ -n "$SUPERUSER_LOGIN" ] && [ -n "$SUPERUSER_PASSWORD" ]; then
+    ./manage.py shell << EOF
+from os import environ
+from django.contrib.auth import get_user_model
+User = get_user_model()
+try:
+    User.objects.get(username=environ["SUPERUSER_LOGIN"])
+except User.DoesNotExist:
+    User.objects.create_superuser(environ["SUPERUSER_LOGIN"], "null@null.com", environ["SUPERUSER_PASSWORD"])
+EOF
+fi
+
 if [ $PRODUCTION -eq 1 ]; then
     gunicorn september1.wsgi:application --bind 0.0.0.0:8000
 else
